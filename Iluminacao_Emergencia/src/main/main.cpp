ca8fe4baa63a988ee1545a25a74f6f548ce2fd4d
User-3396 <alveslucio02@outlook.com>
2026-07-07 18:59:57 +0000

    update

--- Iluminacao_Emergencia/src/main/main.cpp
+++ Iluminacao_Emergencia/src/main/main.cpp
@@ -1,9 +1,8 @@
 #include <Arduino.h>
-#include "../Sensor.cpp"
+// #include "Sensor.cpp"
+#include "Sensor.h"
 
 //> Definição dos pinos de controle: --------------------------
-
-// #define PIN_BTN_CALL 5 // Pino do botao de chamada
 #define PIN_LDR   2 // Pino AO do LDR (variação de luminosidade)
 // #define PIN_LDR2  4 // Pino DO do LDR (simples)
 #define PIN_RELE  4 // Pino IN do Rele
@@ -11,16 +10,23 @@
 const uint8_t pins[]  ={PIN_LDR, PIN_RELE};
 const uint8_t modes[] ={INPUT, OUTPUT};
 
-Sensor ldr; // Instanciando classe do Sensor
+Sensor ldr; // Declarando a classe Sensor
 
-int loopDebounce =300;
+int loopDebounce =300; // Delay geral do programa
 
 void setup (){
   Serial.begin(115200);
   Serial.println("Starting");
   
   Serial.print("Starting LDR: ");
-  ldr.start("corredor-001", pins, modes, 2);
+
+  // Iniciando as configurações do sensor (ldr [Sensor])
+  // Params: 
+  // - id do cliente: "corredor-001"
+  // - pinos: pins
+  // - modos de operação dos pinos: modes
+  // - quantidade de pinos: 2
+  ldr.start("corredor-001", pins, modes, 2); 
 
 }
 
